72d49c64dee7c8014ac7bb9b07ece4da00da409d
Dunn, Simeon <simedunn01@gmail.com>
2024-03-19 15:00:23 -0400

    hero section started

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
     const address string = ":4500";
-    http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+    http.Handle("/assets/*", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
  
     index_page := components.Index();
     http.Handle("/", templ.Handler(index_page)); 
